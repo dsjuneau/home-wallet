504fb5f6d07cf2cdadcaaa3010440563ddbff904
Scott Juneau <dsjuneau@gmail.com>
2019-07-25 17:38:55 -0500

    registration form and login form are set up.  Registration includes verification

--- client/src/components/public/Login.js
+++ client/src/components/public/Login.js
@@ -1,5 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-export default function Login() {
-  return <div>Login screen</div>;
+export class Login extends Component {
+  state = { email: "", password: "" };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.email, this.state.password);
+  };
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            onChange={this.handleChange}
+            name="email"
+            type="text"
+            placeholder="e-mail"
+            value={this.state.email}
+          />
+          <input
+            onChange={this.handleChange}
+            name="password"
+            type="password"
+            placeholder="password"
+            value={this.state.password}
+          />
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
+
+export default Login;
--- client/src/components/public/Register.js
+++ client/src/components/public/Register.js
@@ -1,5 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-export default function Register() {
-  return <div>Registration Screen</div>;
+export class Register extends Component {
+  state = { userName: "", email: "", password: "", password2: "" };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    //verfication block
+    const { userName, email, password, password2 } = this.state;
+    if (userName && email && password) {
+      if (password === password2) {
+        console.log("passed verification");
+      } else {
+        console.log("emial does not match");
+      }
+    } else {
+      console.log("something else did not work");
+    }
+  };
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            onChange={this.handleChange}
+            name="userName"
+            type="text"
+            placeholder="Full Name"
+            value={this.state.userName}
+          />
+          <input
+            onChange={this.handleChange}
+            name="email"
+            type="text"
+            placeholder="e-mail"
+            value={this.state.email}
+          />
+          <input
+            onChange={this.handleChange}
+            name="password"
+            type="password"
+            placeholder="password"
+            value={this.state.password}
+          />
+          <input
+            onChange={this.handleChange}
+            name="password2"
+            type="password"
+            placeholder="retype password"
+            value={this.state.password2}
+          />
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
+
+export default Register;

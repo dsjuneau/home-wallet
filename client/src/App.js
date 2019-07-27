483e08ef2023d23f9586bb7f3334ac3b7f7cc491
Scott Juneau <dsjuneau@gmail.com>
2019-07-27 08:13:30 -0500

    undid that change.  it didnt' really work

--- client/src/App.js
+++ client/src/App.js
@@ -6,32 +6,15 @@ import axios from "axios";
 import "./App.css";
 
 class App extends React.Component {
-  state = { isFromLogin: false, isAuth: false };
+  state = { isAuth: false };
 
-  componentWillMount() {
-    axios.get("/api/auth").then(response => {
-      this.setState({
-        isAuth: response.data.isAuth,
-        userName: response.data.userName
-      });
-    });
-  }
   unAuth = () => {
     this.setState({ isAuth: false });
   };
 
   auth = (userName, id) => {
-    this.setState({ userName, id, isAuth: true, isFromLogin: true });
+    this.setState({ userName, id, isAuth: true });
   };
-
-  componentDidUpdate() {
-    console.log("componentDidUpdate ran");
-    if (this.state.isFromLogin) {
-      console.log("getting here");
-      window.location.assign("/");
-      this.setState({ isFromLogin: false });
-    }
-  }
 
   render() {
     return !this.state.isAuth ? (

6f824ca51b81d3da2be249d9d47ee3485121c0e1
Scott Juneau <dsjuneau@gmail.com>
2019-07-29 16:37:50 -0500

    logout button is working

--- client/src/components/private/Nav.js
+++ client/src/components/private/Nav.js
@@ -2,6 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Nav(props) {
+  const handleClick = () => {
+    document.cookie = `key=;path=/`;
+    props.unAuth();
+  };
+
   return (
     <nav>
       <h1>
@@ -10,6 +15,7 @@ export default function Nav(props) {
         <Link to="/page2/">Page 2</Link>
         <Link to="/page3/">Page 3</Link>
         Welcome {props.userName}
+        <button onClick={handleClick}>Logout</button>
       </h1>
     </nav>
   );

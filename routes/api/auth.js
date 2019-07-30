928e9b0991e231ab3b2fb9e7f18dcc9539eaacd2
Scott Juneau <dsjuneau@gmail.com>
2019-07-30 09:24:09 -0500

    removed some dummy code that was used for testing

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,17 +3,6 @@ const usersController = require("../../controllers/usersController");
 
 require("dotenv").config();
 
-// Matches with "/api/auth"
-// This part of the code emmulates authentication
-router
-  .route("/")
-  .get((req, res) => {
-    res.json({ isAuth: false, userName: "Scott" });
-  })
-  .post((req, res) => {
-    res.json({ isAuth: false, userName: "Scott" });
-  });
-
 router.route("/register").post(usersController.createUser);
 router.route("/login").post(usersController.authUser);
 router.route("/login").get(usersController.isAuthUser);

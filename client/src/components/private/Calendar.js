c4915b271ae7e7204b1d39cdc6c47eb6e9879839
MGM <mgmoser1@users.noreply.github.com>
2019-08-01 19:21:09 -0500

    blank calendar

--- client/src/components/private/Calendar.js
+++ client/src/components/private/Calendar.js
@@ -8,10 +8,30 @@ import './main.scss' // webpack must be configured to do this
 
 export default class DemoApp extends React.Component {
 
-  render() {
-    return (
-      <FullCalendar defaultView="dayGridMonth" plugins={[ dayGridPlugin ]} />
-    )
-  }
+  handleDateClick = (arg) => { // bind with an arrow function
+      alert(arg.dateStr)
+    }
+
+render() {
+  return (
+      <div>
+      <h3>Version 1</h3>
+    <FullCalendar dateClick={this.handleDateClick} defaultView="dayGridMonth" plugins={[ dayGridPlugin ]} />
+
+    {/* <h3>Version 2</h3>
+
+    <FullCalendar
+defaultView="dayGridMonth"
+plugins={calendarPlugins}
+weekends={false}
+events={[
+  { title: 'event 1', date: '2019-04-01' },
+  { title: 'event 2', date: '2019-04-02' }
+]}
+/> */}
+
+    </div>
+  )
+}
 
 }
